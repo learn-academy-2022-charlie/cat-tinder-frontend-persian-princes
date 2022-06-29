c4ad263a2cc36d0c4c68c5f43ee1f915899b0b1b
Renzlawr <RSLogan23@gmail.com>
2022-06-29 17:23:38 -0600

    completed update functionality card

---
+++ src/pages/CatEdit/CatEdit.js
@@ -0,0 +1,98 @@
+import React, { Component } from 'react';
+import {Form, FormGroup, Label, Input, Button} from 'reactstrap'
+import { Redirect } from 'react-router-dom'
+
+class CatEdit extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            updatedCat: {
+                name: "",
+                age: "",
+                enjoys: "",
+                image: "",
+            },
+            submitted: false
+        }
+    }
+
+    componentDidMount = () => {
+        this.setState({updatedCat: this.props.cat})
+    }
+
+    handleChange = (e) => {
+        let targetName = e.target.name
+        let targetValue = e.target.value
+        let { updatedCat } = this.state
+        updatedCat[targetName] = targetValue
+        this.setState({updatedCat: updatedCat})
+    }
+
+    handleSubmit = () => {
+        this.props.editCat(this.state.updatedCat)
+        this.setState({submitted: true})
+    }
+
+    render() {
+        if (this.state.submitted) {
+            return (<Redirect to={'/catindex'} />);
+        }
+        return (
+            <>
+                <h1>Update your card!</h1>
+                <Form>
+                    <FormGroup>
+                        <Label for="name">
+                            Name
+                        </Label>
+                        <Input
+                            name="name"
+                            type="text"
+                            onChange={this.handleChange}
+                            value={this.state.updatedCat.name}
+                        />
+                    </FormGroup>
+                    <FormGroup>
+                        <Label for="age">
+                            Age
+                        </Label>
+                        <Input
+                            name="age"
+                            type="text"
+                            onChange={this.handleChange}
+                            value={this.state.updatedCat.age}
+                        />
+                    </FormGroup>
+                    <FormGroup>
+                        <Label for="enjoys">
+                            Enjoys
+                        </Label>
+                        <Input
+                            name="enjoys"
+                            type="text-area"
+                            onChange={this.handleChange}
+                            value={this.state.updatedCat.enjoys}
+                        />
+                    </FormGroup>
+                    <FormGroup>
+                        <Label for="image">
+                            Image
+                        </Label>
+                        <Input
+                            name="image"
+                            type="text"
+                            onChange={this.handleChange}
+                            value={this.state.updatedCat.image}
+                        />
+                    </FormGroup>
+                        <Button onClick={this.handleSubmit}>
+                            Submit
+                        </Button>
+                </Form>
+            </>
+        )
+    }
+    
+}
+
+export default CatEdit;
