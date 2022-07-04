90ae8bd897c147b263e4cfc746c60427a1801b1a
jennerdulce <jennerdulce@gmail.com>
2022-07-04 09:26:02 -0700

    Modal working

--- src/components/Modal/ModalComponent.js
+++ src/components/Modal/ModalComponent.js
@@ -1,23 +1,20 @@
 import React from 'react';
 import {Modal, Button} from 'react-bootstrap'
+import './styles.module.css'
+import '../Card/Card.css'
 
 
 function ModalComponent(props) {
     return (
         <Modal
           {...props}
-          size="lg"
+          size="md"
           aria-labelledby="contained-modal-title-vcenter"
           centered
         >
-          <Modal.Header closeButton>
-            <Modal.Title id="contained-modal-title-vcenter" className="modal-name">
-              {props.cat.name}
-            </Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
+          <Modal.Body id="modal-container">
             <img id="modal-image" src={props.cat.image}></img>
-            <h4 id="modal-age">{props.cat.age}</h4>
+            <h4 id="modal-info"><span>{props.cat.name}</span>, {props.cat.age}</h4>
             <p id="modal-enjoys">
               {props.cat.enjoys}
             </p>
